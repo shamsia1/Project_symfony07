25bfab185979bfdc7aae6823ac4b4e165973a210
shamsia1 <alimishamsya@gmail.com>
2019-05-21 14:35:14 +0200

    bidrection

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,62 +5,18 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\ArticlleRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
  */
 class Article
 {
+    // ...
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $title;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $content;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="Category")
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="articles")
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
-
-    public function setContent(string $content): self
-    {
-        $this->content = $content;
-
-        return $this;
-    }
-
+    //getters and setters
     public function getCategory(): ?Category
     {
         return $this->category;
@@ -69,7 +25,6 @@ class Article
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
-
         return $this;
     }
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,38 +3,64 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-
+use phpDocumentor\Reflection\Types\Self_;
+use Symfony\Component\Validator\Constraints\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 /**
- * @ORM\Entity(repositoryClass="App\Repository\CategoriRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
  */
 class Category
 {
+    //...
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
+     * @ORM\OneToMany(targetEntity="App\Entity\Article", mappedBy="Category")
      */
-    private $id;
+    private $articles;
 
-    /**
-     * @ORM\Column(type="string", length=100)
-     */
-    private $name;
+public function__construct()
+{
+$this->articles = new ArrayCollection();
+}
 
-    public function getId(): ?int
+//...
+/**
+ * @return Collection|Article[]
+ */
+public
+function getArticles(): Collection
+{
+    return $this->articles;
+}
+
+public
+function addArticle(Article $article): Self
+{
+    if (!$this->articles->contains($article)) {
+        $this->articles[] = $article;
+        $article->setCategory($this);
+    }
+    return $this;
+}
+
+
+/**
+ * @param Article $article
+ * @return Category
+ */
+public
+function removeArticle(Article $article): Self
     {
-        return $this->id;
+    if ($this->articles->contains($article)) {
+        $this->articles->removeElement($article);
+        //set the owning side to null(unless already changed)
+        if ($article->getCategory() === $this) {
+            $article->setCategory(null);
+        }
     }
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
+    return $this;
     }
 }
+
+
+
